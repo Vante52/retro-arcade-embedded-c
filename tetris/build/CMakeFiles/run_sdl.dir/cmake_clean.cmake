1c90f380214437b936f422b2c8559877acb42884
Valka <val3kta@gmail.com>
2026-06-03 14:47:08 -0500

    style: Clean up tracked IDE and build artifacts

--- tetris/build/CMakeFiles/run_sdl.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/run_sdl"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/run_sdl.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
